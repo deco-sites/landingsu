5a0e6a1d7ac43057f43b0a1b65c7d6502b74aebe
Rodrigo Eloy <rodrigoecavalcanti@gmail.com>
2024-05-15 19:22:47 +0000

    Release staging environment with 1 changes made by Rodrigo Eloy

--- sections/ImageWithParagraph.tsx
+++ sections/ImageWithParagraph.tsx
@@ -12,20 +12,9 @@ export interface Props {
   title?: string;
   /** @format textarea */
   description?: string;
-  tagline?: string;
   image?: ImageWidget;
-  placement?: "left" | "right";
   cta?: CTA;
-  disableSpacing?: {
-    top?: boolean;
-    bottom?: boolean;
-  };
 }
-
-const PLACEMENT = {
-  left: "flex-col md:flex-row-reverse",
-  right: "flex-col md:flex-row",
-};
 
 const DEFAULT_IMAGE =
   "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4763/772e246e-1959-46ac-a309-3f25ab20af6f";
@@ -34,29 +23,26 @@ export default function ImageWithParagraph({
   title = "Here's an intermediate size heading you can edit",
   description =
   "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
-  tagline = "Tagline",
   image = DEFAULT_IMAGE,
-  placement = "left",
-  disableSpacing,
   cta = { id: "campaign-gallery", href: "/", text: "Campaign Gallery" },
 }: Props) {
   return (
-    <div class="flex justify-center lg:container md:max-w-6xl lg:mx-auto mx-4 text-sm">
-      <div class="flex flex-col items-center gap-8 w-full md:w-1/2 border border-secondary rounded-lg overflow-hidden">
-        <p class="text-4xl leading-snug text-center">
+    <div class="flex justify-center py-16 lg:container md:max-w-6xl lg:mx-auto mx-4 text-sm">
+      <div class="flex flex-col items-center gap-8 w-full md:w-3/4 border border-secondary rounded-lg overflow-hidden">
+        <p class="text-2xl leading-snug text-center">
           {title}
         </p>
         <Image
-          width={640}
+          width={800}
           height={400}
-          class="object-fit z-10"
+          class="object-fit z-10 border border-8 border-accent rounded-lg"
           sizes="(max-width: 100vw) 100vw, 30vw"
           src={image}
           alt={image}
           decoding="async"
           loading="lazy"
         />
-        <p class="leading-normal text-center">
+        <p style="button" class="leading-6 text-cente r">
           {description}
         </p>
         <a class="w-full max-w-72 font-normal btn btn-primary btn-outline" id={cta.id} href={cta.href}>{cta.text}</a>
